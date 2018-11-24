487d47241a04db8b368e1c6eaac14556dcac0a37
ihollander <ianholla@gmail.com>
2018-11-24 12:25:59 -0500

    more features

--- src/domController.js
+++ src/domController.js
@@ -1,13 +1,15 @@
-function updateDOM(strValue) {
-  const identicon = new Identicon(strValue)
+function updateDOM(container, strValue, gridSize, gridLayout) {
+  container.innerHTML = ''
+  container.style.gridTemplateColumns = `repeat(${gridSize}, ${100 / gridSize}%)`
+  container.style.gridTemplateRows = `repeat(${gridSize}, ${100 / gridSize}%)`
+
+  const identicon = new Identicon(strValue, gridSize, gridLayout)
   identicon.grid.forEach((row, rowIndex) => {
     row.forEach((cell, colIndex) =>{
-      let block = document.getElementById(`${rowIndex}-${colIndex}`)
-      if (cell) {
-        block.style.backgroundColor = identicon.colorRGB
-      } else {
-        block.style = null
-      }
+      let block = document.createElement('span')
+      block.id = `${rowIndex}-${colIndex}`
+      block.style.backgroundColor = cell ? identicon.colorRGB : null
+      container.appendChild(block)
     })
   })
 }
--- src/identicon.js
+++ src/identicon.js
@@ -1,11 +1,11 @@
+// gridMaker classes
 class Identicon {
   // make use of the md5.array method, which is in scope because we required it in our index.html
-  constructor(strValue) {
+  constructor(strValue, gridSize, gridLayout) {
     this.strValue = strValue
-    this.gridSize = 5 // 5 x 5 grid
-    this.mirroredSquaresPerRow = Math.ceil(this.gridSize / 2) // 3 columns per grid (two mirrored)
-    this.mirroredSquares = this.gridSize * this.mirroredSquaresPerRow // 15 mirrored squares
-    this.md5array = md5.array(strValue).slice(0, this.mirroredSquares) // only take first 15 elements of array (so much math)
+    this.gridSize = gridSize
+    this.gridLayout = gridLayout
+    this.md5array = md5.array(strValue)
     this.colors = this.getColors()
     this.colorRGB = `rgb(${this.colors.join(",")})`
     this.grid = this.getGrid()
@@ -16,25 +16,23 @@ class Identicon {
     return this.md5array.slice(0,3)
   }
 
-  // returns 2D array of booleans to create blocks 
+  // calls grid class to make grid
   getGrid() {
-    let gridArray = []
-    this.md5array.forEach((number, index) => { // 15 element array
-      let val = number % 2 === 0 ? true : false // even/odd values
-      let rowIndex = Math.floor(index / this.mirroredSquaresPerRow)
-      if (!gridArray[rowIndex]) {
-        gridArray[rowIndex] = [] // create new array for new rows
-      }
-      let colIndex = index % this.mirroredSquaresPerRow
-
-      // do mirroring splice fun
-      // spread array for doubled values
-      if (colIndex + 1 === this.mirroredSquaresPerRow) {
-        gridArray[rowIndex].splice(colIndex, 0, val)
-      } else {
-        gridArray[rowIndex].splice(colIndex, 0, val, val)
-      }
-    })
-    return gridArray
+    let grid;
+    switch (this.gridLayout) {
+      case 'horizontal':
+        grid = new HorizontallyMirroredGrid(this.md5array, this.gridSize)
+        break
+      case 'vertical':
+        grid = new VerticallyMirroredGrid(this.md5array, this.gridSize)
+        break
+      case 'rotational':
+        grid = new RotationallyMirroredGrid(this.md5array, this.gridSize)
+        break
+      default:
+        grid = new HorizontallyMirroredGrid(this.md5array, this.gridSize)
+        break
+    }
+    return grid.gridArray
   }
 }
