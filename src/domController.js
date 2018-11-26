af7d276bc4ea0d339625c9be2e0916adf40550e3
brewchetta <chett.tiller@gmail.com>
2018-11-26 15:15:41 -0500

    Done.

--- src/domController.js
+++ src/domController.js
@@ -1,3 +1,24 @@
-function updateDOM() {
+identiconDOMSquares = ['0-0','0-1','0-2','1-0','1-1','1-2','2-0','2-1','2-2','3-0','3-1','3-2','4-0','4-1','4-2']
 
+function updateDOM() {
+  Array.from(document.getElementById('identicon').children).forEach((square) => {
+    square.style = 'background: white'
+  })
+
+  for (let i = 0; i < identiconDOMSquares.length; i++) {
+    console.log('i:', i)
+    const filled = !!newIdenticon[`val${i}`]
+    console.log('Square filled:', filled)
+    if (filled) {
+      document.getElementById(identiconDOMSquares[i]).style = `background: ${color}`
+      if (identiconDOMSquares[i][2] == 0) {
+        document.getElementById(`${identiconDOMSquares[i][0]}-4`).style = `background: ${color}`
+      } else if (identiconDOMSquares[i][2] == 1) {
+        document.getElementById(`${identiconDOMSquares[i][0]}-3`).style = `background: ${color}`
+      } // end if /elsif
+    } // end if
+  } // end for
 }
+
+
+// document.getElementById('0-0').style = `background: ${color}`
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-function handleSubmit(e) {
+newIdenticon = new Identicon()
+let color = ''
 
+function handleSubmit(e) {
+  e.preventDefault()
+  const idenString = document.querySelector('#identicon-form-field').value
+  const hashedIden = md5.array(idenString)
+  color = `rgb(${hashedIden[0]},${hashedIden[1]},${hashedIden[2]})`
+
+  for (let i = 0; i < hashedIden.length; i ++) {
+    newIdenticon[`val${i}`] = hashedIden[i] % 2
+  }
+  if (idenString.length > 0) {
+    updateDOM()
+  }
+
+  document.querySelector('#identicon-form-field').value = ''
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
   form.addEventListener("submit", handleSubmit)
 })
+
+console.log('loaded index.js')
