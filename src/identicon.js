e29a3eb5d949e225d3a0aa0b7cf52ab57f1ead13
Veraprot <vera.protopopova@gmail.com>
2018-11-26 11:55:15 -0500

    Done.

--- src/identicon.js
+++ src/identicon.js
@@ -1,3 +1,30 @@
 class Identicon {
-  // make use of the md5.array method, which is in scope because we required it in our index.html
+  constructor() {
+    this.boardCells = [];
+    this.color = '#'; 
+  }
+
+  generateCells() {
+    let cellsHTML = Array.from(document.getElementById('identicon').children);
+    cellsHTML.forEach( cell => {
+      this.boardCells.push({active: Math.floor(Math.random() * 2), id: cell.id});
+    });
+  }
+
+  generateRandomColor() {
+    let hexLetters = '0123456789ABCDEF';
+    for(let i = 0; i < 6; i++) {
+      this.color += hexLetters[Math.floor(Math.random() * 16)];
+    }
+    
+    this.boardCells.forEach( cell => {
+      let cellBox = document.getElementById(cell.id)
+      if(cell.active) {
+        cellBox.style = `background-color: ${this.color}`;
+      }
+      else {
+        cellBox.style = "";
+      }
+    });
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-function handleSubmit(e) {
+identiconsHash = {};
 
+function handleSubmit(e) {
+  e.preventDefault();
+  // console.log(e.target[0].value);
+  if (e.target[0].value.length > 0) {
+    for(key in identiconsHash) {
+      console.log('this here hi');
+      if(identiconsHash[key]) {
+        console.log('this identicon exists')
+      }
+      else {
+        let identicon = new Identicon();
+        identicon.generateCells();
+        identicon.generateRandomColor();
+
+        identiconsHash[e.target[0].value] = identicon;
+      }
+    }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
   form.addEventListener("submit", handleSubmit)
-})
+});
