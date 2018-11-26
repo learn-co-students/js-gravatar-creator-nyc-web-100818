d2650cb1723bc53b35c8c7037a9277b549fc4653
Veraprot <vera.protopopova@gmail.com>
2018-11-26 12:04:47 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,20 @@ identiconsHash = {};
 
 function handleSubmit(e) {
   e.preventDefault();
-  // console.log(e.target[0].value);
   if (e.target[0].value.length > 0) {
-    for(key in identiconsHash) {
-      console.log('this here hi');
-      if(identiconsHash[key]) {
-        console.log('this identicon exists')
-      }
-      else {
-        let identicon = new Identicon();
-        identicon.generateCells();
-        identicon.generateRandomColor();
+    console.log('this here hi');
+    if(identiconsHash.hasOwnProperty(e.target[0].value)) {
+      console.log('this identicon exists')
+    }
+    else {
+      let identicon = new Identicon();
+      identicon.generateCells();
+      identicon.generateRandomColor();
 
-        identiconsHash[e.target[0].value] = identicon;
-      }
+      identiconsHash[e.target[0].value] = identicon;
     }
   }
+  console.log(identiconsHash);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
